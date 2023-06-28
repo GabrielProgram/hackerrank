fa279fba731996d7658abc429e6883397743575f
GabrielProgram <64813945+GabrielProgram@users.noreply.github.com>
2023-06-28 09:57:41 -0300

    Create 0001_query_column_usa_cities_population_larger_than_100000.sql
    First HackerRank SQL challenge. Query which USA cities have more than 100,000 population

---
+++ SQL/0001_query_column_usa_cities_population_larger_than_100000.sql
@@ -0,0 +1,25 @@
+--First HackerRank Challange?
+
+--Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+--The CITY table is described as follows:
+
+--|Field        |Type        |
+--|ID           |NUMBER      |
+--|NAME         |VARCHAR(17) |
+--|CONTRYCODE   |VARCHAR(3)  |
+--|DISTRICT     |VARCHAR(20) |
+--|POPULATION   |NUMBER      |
+
+SELECT * FROM CITY
+WHERE CountryCode = 'USA' AND POPULATION > 100000
+
+--Compiler Message
+--Success
+--Input (stdin)
+ 
+--Expected Output
+--3878 Scottsdale USA Arizona 202705 
+--3965 Corona USA California 124966 
+--3973 Concord USA California 121780 
+--3977 Cedar Rapids USA Iowa 120758 
+--3982 Coral Springs USA Florida 117549 
