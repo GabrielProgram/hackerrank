4a55a7f4f51c87341ba51881ed42f6c76b9eb413
GabrielProgram <64813945+GabrielProgram@users.noreply.github.com>
2023-06-28 10:21:44 -0300

    Create 0003_query_all_columns_for_every_row_n_the_city_table.sql
    Third HackerRank challenge, select all from table city

---
+++ SQL/0003_query_all_columns_for_every_row_n_the_city_table.sql
@@ -0,0 +1,33 @@
+--3rd HackerRank Challange:
+
+--Query all columns (attributes) for every row in the CITY table.
+--The CITY table is described as follows:
+--|Field        |Type        |
+--|ID           |NUMBER      |
+--|NAME         |VARCHAR(17) |
+--|CONTRYCODE   |VARCHAR(3)  |
+--|DISTRICT     |VARCHAR(20) |
+--|POPULATION   |NUMBER      |
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+SELECT * FROM CITY;
+
+--Compiler Message
+--success
+--Input (stdin)
+ 
+--Expected Output
+--6 Rotterdam NLD Zuid-Holland 593321 
+--3878 Scottsdale USA Arizona 202705 
+--3965 Corona USA California 124966 
+--3973 Concord USA California 121780 
+--3977 Cedar Rapids USA Iowa 120758 
+--3982 Coral Springs USA Florida 117549 
+--4054 Fairfield USA California 92256 
+--4058 Boulder USA Colorado 91238 
+--4061 Fall River USA Massachusetts 90555 
